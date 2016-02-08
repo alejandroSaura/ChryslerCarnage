10953d96fc107657ef519a33fcf50622ff0f4e87
alejandroSaura <alejandro.saura.villanueva@gmail.com>
2016-02-08 01:55:30 +0000

    - xbox controller and keyboard supported - basic acceleration-brake - tangential weight transfer

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,13 +4,23 @@ using System.Collections;
 public class CarController : MonoBehaviour {
 
 	// public
-	public float engineForce;
+	public float enginePower;
+	public float brakePower;
 	public float dragConstant;
 	public float rollConstant;
 
 	// private
+
+	Transform CenterOfMass;
+	Transform FrontAxis;
+	Transform RearAxis;
+
 	Rigidbody _rigidbody;
 	Vector3 resultForce;
+
+	public float frontWeight;
+	public float rearWeight;
+	Vector3 gizmoPosition;
 
 	// user controls
 	public float userThrottle;
@@ -27,26 +37,57 @@ public class CarController : MonoBehaviour {
 	void Start () 
 	{
 		_rigidbody = GetComponent<Rigidbody>();
-	}
-
-	public void setEngineForce(float newEngineForce)
-	{
-		engineForce = newEngineForce;
+		CenterOfMass = transform.FindChild("CenterOfMass");
+		FrontAxis = transform.FindChild("FrontAxis");
+		RearAxis = transform.FindChild("RearAxis");
 	}
 
 	void OnGUI()
 	{
 		float velocity_forward = Vector3.Dot(_rigidbody.velocity, transform.forward);
 
-		// Make a background box
 		GUI.Box(new Rect(10,10,300,100), "Debug Data");
-
 		GUI.TextArea(new Rect(15,30,200,40), "Forward velocity = " + velocity_forward*3.6f +" km/h");
 	}
 
 	void Update()
 	{
 		getUserInput();
+
+		// calculate weight dynamic transfer ----------------------
+
+		Vector3 CenterOfMassAligned = CenterOfMass.localPosition; // put the three objects in the same plane (y=0)
+		CenterOfMassAligned.y = 0.0f;
+		Vector3 FrontAxisAligned = FrontAxis.localPosition;
+		FrontAxisAligned.y = 0.0f;
+		Vector3 RearAxisAligned = RearAxis.localPosition;
+		RearAxisAligned.y = 0.0f;
+
+		float distanceToFront = (CenterOfMassAligned - FrontAxisAligned).magnitude;
+		float distanceToRear = (CenterOfMassAligned - RearAxisAligned).magnitude;
+		float wheelBase = (FrontAxisAligned - RearAxisAligned).magnitude;
+
+		Vector3 acceleration = resultForce/_rigidbody.mass;
+		float tangentialAcceleration = transform.InverseTransformDirection(acceleration).z;
+
+		frontWeight = (distanceToRear/wheelBase) * _rigidbody.mass*Physics.gravity.y + (CenterOfMass.localPosition.y/wheelBase) * _rigidbody.mass * tangentialAcceleration;
+		rearWeight = (distanceToFront/wheelBase) * _rigidbody.mass*Physics.gravity.y - (CenterOfMass.localPosition.y/wheelBase) * _rigidbody.mass * tangentialAcceleration;
+
+
+		// for debugging
+		float weight = _rigidbody.mass*Physics.gravity.y;
+		float frontWeightPercent = frontWeight/weight;
+		float rearWeightPercent = rearWeight/weight;
+		gizmoPosition = FrontAxis.position*frontWeightPercent + RearAxis.position*rearWeightPercent;
+
+		// ----------------------------------------------------------
+	}
+
+	void OnDrawGizmos()
+	{
+		// Debugging gizmos
+		Gizmos.color = Color.blue;
+		Gizmos.DrawSphere( gizmoPosition + new Vector3(0.0f, 0.2f, 0.0f), 0.4f);
 	}
 	
 	void FixedUpdate () 
@@ -55,7 +96,8 @@ public class CarController : MonoBehaviour {
 
 		// Calculate forces
 
-		Vector3 tractionForce = transform.forward * engineForce;
+		Vector3 tractionForce = transform.forward * enginePower * userThrottle; //TO-DO: don't affect engineForce with direct user input
+		Vector3 brakeForce = - transform.forward * brakePower * userBrake;
 
 		Debug.DrawLine(transform.position, transform.position + tractionForce/200.0f, Color.green);
 
@@ -66,6 +108,9 @@ public class CarController : MonoBehaviour {
 		Debug.DrawLine(transform.position, transform.position + rollingForce/200.0f, Color.yellow);
 
 		resultForce = tractionForce + dragForce + rollingForce;
+
+		if(velocity_forward > 0.1f) resultForce += brakeForce;
+		if(Mathf.Abs(velocity_forward) < 0.1f && userBrake > 0.0f) _rigidbody.velocity = Vector3.zero; // to avoid small slidings
 
 		// Apply forces
 		_rigidbody.AddForce(resultForce);
