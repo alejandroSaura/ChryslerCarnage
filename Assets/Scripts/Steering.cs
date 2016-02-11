f04b6f65d213d536330c3c0a7e35046fe95657b4
alejandroSaura <alejandro.saura.villanueva@gmail.com>
2016-02-11 22:51:57 +0000

    some scene adjustments

---
+++ Assets/Scripts/Steering.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Steering : MonoBehaviour {
+
+    Transform wheelGeometry;
+
+    // Use this for initialization
+    void Start ()
+    {
+        wheelGeometry = transform.FindChild("wheel");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Quaternion rot = Quaternion.Euler(50.0f, 0.0f, 0.0f);
+        wheelGeometry.rotation.Set(rot.x, rot.y, rot.z, rot.w);
+    }
+}
