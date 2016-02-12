a96cae9516039e48df27a271633e1c0a7845e187
alejandroSaura <alejandro.saura.villanueva@gmail.com>
2016-02-12 13:35:32 +0000

    adjustments

--- Assets/Scripts/WheelExperiment.cs
+++ Assets/Scripts/WheelExperiment.cs
@@ -24,6 +24,7 @@ public class WheelExperiment : MonoBehaviour
 
     public AnimationCurve slipCurve;
     public AnimationCurve tractionCurve;
+    public AnimationCurve userThrottleWeight;
 
 
     public Rigidbody carRB;
@@ -35,12 +36,19 @@ public class WheelExperiment : MonoBehaviour
     public float angularVelocityFactor;
     public float tractionFactor;
 
+    public float meanWeightSupported;
+    public float weightFactor;
+
+    Vector4 slipColor;
+    Vector4 tractionColor;
+
 
     void Start()
     {
         wheelGeometry = transform.FindChild("wheel");
         angularVelocity = 0;
         //_rigidbody = wheelGeometry.GetComponent<Rigidbody>();
+        meanWeightSupported = transform.parent.gameObject.GetComponent<Rigidbody>().mass*Physics.gravity.y/4.0f;
 
         input = transform.parent.gameObject.GetComponent<InputInterface>();
     }
@@ -50,18 +58,34 @@ public class WheelExperiment : MonoBehaviour
 
     }
 
-    void FixedUpdate()
+    void OnDrawGizmos()
     {
+        // Debugging gizmos
+        Gizmos.color = slipColor;
+        Gizmos.DrawSphere(transform.position + new Vector3(0.0f, 0.6f, 0.0f), 0.2f);
+
+        Gizmos.color = tractionColor;
+        Gizmos.DrawSphere(transform.position + new Vector3(0.0f, 1f, 0.0f), 0.2f);
+    }
+
+        void FixedUpdate()
+    {
+        weightFactor = -((supportedWeight - meanWeightSupported) / meanWeightSupported) * 5.0f;
+        if (weightFactor < -1) weightFactor = -1;
+
         // get the slip ratio
         //wheelLinearVelocity = transform.InverseTransformDirection(_rigidbody.velocity).z;
         carLinearVelocity = transform.InverseTransformDirection(carRB.velocity).z;
-        wheelLinearVelocity = angularVelocity * wheelRadius;
+        wheelLinearVelocity = angularVelocity * wheelRadius;       
 
-        angularVelocityFactor = 1 + slipCurve.Evaluate(angularVelocity) * input.userThrottle * 0.5f;
-        angularVelocity = angularVelocityFactor  * carLinearVelocity/wheelRadius;
+        // loss of traction at low speed to simulate slip, then depends on the supported weight
+        tractionFactor = (1 - tractionCurve.Evaluate(angularVelocity)) * (1-weightFactor) ;        
+
+        //depends on traction and its own curve.
+        angularVelocityFactor = 1 + slipCurve.Evaluate(angularVelocity) * userThrottleWeight.Evaluate(input.userThrottle) * Mathf.Abs(1-tractionFactor*0.5f);
+        angularVelocity = angularVelocityFactor * carLinearVelocity / wheelRadius;
         if (driveTorque == 0) angularVelocity = carLinearVelocity / wheelRadius;
 
-        tractionFactor = 1 - (tractionCurve.Evaluate(angularVelocity) * input.userThrottle);
         //if (angularVelocityFactor > 0)
         //{
         //    tractionFactor = 1-(angularVelocityFactor - (int)angularVelocityFactor);            
@@ -78,6 +102,10 @@ public class WheelExperiment : MonoBehaviour
         // Apply forces to the car
         carRB.AddForce(tractionTorque / wheelRadius * transform.forward);
         // Rotate the geometry
-        wheelGeometry.Rotate(0.0f, angularVelocity * Mathf.Rad2Deg * Time.fixedDeltaTime, 0.0f);        
+        wheelGeometry.Rotate(0.0f, angularVelocity * Mathf.Rad2Deg * Time.fixedDeltaTime, 0.0f);
+
+
+        slipColor = new Vector4(1, 1-(angularVelocityFactor-1), 1-(angularVelocityFactor-1), 1);  
+        tractionColor = new Vector4(1-tractionFactor, 1-tractionFactor, 1, 1);
     }
 }
