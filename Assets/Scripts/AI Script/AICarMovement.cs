736203eb71152358a75f4fec9fb983b9f40338eb
Tin-Tin <kt-chin@hotmail.com>
2016-03-18 16:24:58 +0000

    Working AI with steering

--- Assets/Scripts/AI Script/AICarMovement.cs
+++ Assets/Scripts/AI Script/AICarMovement.cs
@@ -3,14 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class AICarMovement : InputInterface {
-    //public List<Transform> path;
-    //public Transform pathgroup;
+    public List<Transform> path;
+    public Transform pathgroup;
     public int currentPathNode;
-    float maxSteer = 15.0f;
+    public float maxSteer = 3.0f;
     double currentSpeed;
     double topSpeed = 175.0;
-    public float distFromPath;
-    carPathMover pathReference;
+    float distFromPath = 100.0f;
+    float stabilizer = 0.05f;
+    //carPathMover pathReference;
     Vector3 maxPos;
     Vector3 minPos;
     Vector3 offsetPos;
@@ -35,7 +36,7 @@ public class AICarMovement : InputInterface {
         
        //  Debug.DrawLine(transform.position, transform.position + transform.forward * 10 ,Color.blue);
       //  currentPosition = transform.position;
-        //transform.LookAt(pathReference.path);
+        //transform.LookAt(path);
     }
     void OnGUI()
     {
@@ -52,17 +53,17 @@ public class AICarMovement : InputInterface {
 
     void getPath()
     {
-        Transform[] path_objs = pathReference.pathgroup.GetComponentsInChildren<Transform>(); //creates array of path nodes in the children
-        pathReference.path = new List<Transform>();
+        Transform[] path_objs = pathgroup.GetComponentsInChildren<Transform>(); //creates array of path nodes in the children
+        path = new List<Transform>();
 
         for (int i = 0; i < path_objs.Length; i++)
         {
-            if (path_objs[i] != pathReference.pathgroup)
+            if (path_objs[i] != pathgroup)
             {
-                pathReference.path.Add(path_objs[i]);
+                path.Add(path_objs[i]);
             }
         }
-        Debug.Log(pathReference.path.Count);
+        Debug.Log(path.Count);
     }
     void Move()
     {
@@ -83,14 +84,21 @@ public class AICarMovement : InputInterface {
     }
     void steering()
     {
-        Vector3 steerVector = transform.InverseTransformPoint(new Vector3(pathReference.path[currentPathNode].position.x, transform.position.y, pathReference.path[currentPathNode].position.z));
+        Vector3 steerVector = transform.InverseTransformPoint(new Vector3(path[currentPathNode].position.x, transform.position.y, path[currentPathNode].position.z));
         float newSteer = maxSteer * (steerVector.x / steerVector.magnitude);
+        newSteer = newSteer / 10;
+        //if(newSteer <= 0)
+        //{
+        //    newSteer += stabilizer;
+        //}
+        //else { newSteer -= stabilizer; }
         userLeftStickHorizontal = newSteer;
-        Debug.Log(newSteer);
+        
+       // Debug.Log(newSteer);
         if (steerVector.magnitude <= distFromPath)
         {
             currentPathNode++;
-            if (currentPathNode >= pathReference.path.Count)
+            if (currentPathNode >= path.Count)
             {
                 currentPathNode = 0;
             }
--- Assets/Scripts/AI Script/carPathMover.cs
+++ Assets/Scripts/AI Script/carPathMover.cs
@@ -17,8 +17,8 @@ public class carPathMover : MonoBehaviour
     Rigidbody aiRigidBody;
     
     
-    //public PhysicsWheel FLwheel;
-    //public PhysicsWheel FRwheel;
+    public PhysicsWheel FLwheel;
+    public PhysicsWheel FRwheel;
     //public PhysicsWheel RLwheel;
     //public PhysicsWheel RRwheel;
     public float dir;
@@ -49,53 +49,52 @@ public class carPathMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Steering();
-        Move();
+
     }
 
-    void Steering()
-    {
-        Vector3 steerVector = transform.InverseTransformPoint(new Vector3(path[currentPathNode].position.x, transform.position.y, path[currentPathNode].position.z));
-        float newSteer = maxSteer * (steerVector.x / steerVector.magnitude);
-        dir = steerVector.x / steerVector.magnitude;
-        // referenceWheel.wheelSteerAngleTarget = newSteer;
-        //FLwheel.wheelSteerAngleTarget = newSteer;
-        //FRwheel.wheelSteerAngleTarget = newSteer;
+    //void Steering()
+    //{
+    //    Vector3 steerVector = transform.InverseTransformPoint(new Vector3(path[currentPathNode].position.x, transform.position.y, path[currentPathNode].position.z));
+    //    float newSteer = maxSteer * (steerVector.x / steerVector.magnitude);
+    //    dir = steerVector.x / steerVector.magnitude;
+    //    // referenceWheel.wheelSteerAngleTarget = newSteer;
+    //    //FLwheel.wheelSteerAngleTarget = newSteer;
+    //    //FRwheel.wheelSteerAngleTarget = newSteer;
 
-        if (steerVector.magnitude <= distFromPath)
-        {
-            currentPathNode++;
-            if (currentPathNode >= path.Count)
-            {
-                currentPathNode = 0;
-            }
-        }
-    }
+    //    if (steerVector.magnitude <= distFromPath)
+    //    {
+    //        currentPathNode++;
+    //        if (currentPathNode >= path.Count)
+    //        {
+    //            currentPathNode = 0;
+    //        }
+    //    }
+    //}
 
-    void Move()
-    {
-       // currentSpeed = 2 * (22 / 7) * RLwheel.wheelRadius * RLwheel.angularVelocity * 60 / 10;
-        //currentSpeed = Mathf.Round(currentSpeed);
-        if (currentSpeed <= topSpeed)
-        {
-            //RLwheel.driveTorque = maxTorque;
-            //RRwheel.driveTorque = maxTorque;
-            //FLwheel.driveTorque = maxTorque;
-            //FRwheel.driveTorque = maxTorque;
-            //RLwheel.brakeTorque = 0;
-            //RRwheel.brakeTorque = 0;
-            //FLwheel.brakeTorque = 0;
-            //FRwheel.brakeTorque = 0;
-        }
-        else
-        {
-            //RLwheel.driveTorque = 0;
-            //RRwheel.driveTorque = 0;
-            //RLwheel.brakeTorque = decellerationSpeed;
-            //RRwheel.brakeTorque = decellerationSpeed;
+    //void Move()
+    //{
+    //      //currentSpeed = 2 * (22 / 7) * RLwheel.wheelRadius * RLwheel.angularVelocity * 60 / 10;
+    //      //currentSpeed = Mathf.Round(currentSpeed);
+    //    if (currentSpeed <= topSpeed)
+    //    {
+    //        //RLwheel.driveTorque = maxTorque;
+    //        //RRwheel.driveTorque = maxTorque;
+    //        //FLwheel.driveTorque = maxTorque;
+    //        //FRwheel.driveTorque = maxTorque;
+    //        //RLwheel.brakeTorque = 0;
+    //        //RRwheel.brakeTorque = 0;
+    //        //FLwheel.brakeTorque = 0;
+    //        //FRwheel.brakeTorque = 0;
+    //    }
+    //    else
+    //    {
+    //        //RLwheel.driveTorque = 0;
+    //        //RRwheel.driveTorque = 0;
+    //        //RLwheel.brakeTorque = decellerationSpeed;
+    //        //RRwheel.brakeTorque = decellerationSpeed;
 
-        }
-    }
+    //    }
+    //}
 }
 
 
