b99f94382c64f7b5473abb5ce2b57e637f7c59e5
Tin-Tin <kt-chin@hotmail.com>
2016-04-23 20:03:51 +0100

    Need to Fix Particle Emission + collision point detection

---
+++ Assets/Scripts/Destruction/DamageParticle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageParticle : MonoBehaviour {
+    private ParticleSystem smokeParticle;
+    public DamageScript hpRef;
+	// Use this for initialization
+	void Start () {
+        smokeParticle = GetComponent<ParticleSystem>();
+        hpRef = GetComponent<DamageScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        smokeParticle.maxParticles = Mathf.RoundToInt(100-hpRef.carHP);//Mathf.RoundToInt(hpRef.carHP)
+        //smokeParticle.maxParticles = 10;
+	}
+}
---
+++ Assets/Scripts/Destruction/DamageScript.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageScript : MonoBehaviour {
+    public float carHP=100.0f;
+    public Transform collisionParticle;
+    public GameObject damagedParticle;
+    public Transform DeathParticle;
+    public GameObject mainCamera;
+    public GameObject carObject;
+    public float impactAmount;
+	// Use this for initialization
+	void Start () {
+        damagedParticle.SetActive(false);
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        //if (col.gameObject.tag == "FrontBumper")
+        //{
+        //    if (col.relativeVelocity.magnitude > 25)
+        //    {
+        //        GameObject.FindGameObjectsWithTag("FrontBumper").SetActive(false);
+        //    }
+        //}
+        Vector3 dir = (col.gameObject.transform.position - gameObject.transform.position).normalized;
+        if (Mathf.Abs(dir.z) < 0.05f)
+        {
+            if(dir.x > 0)
+            {
+                Debug.Log("RIGHT");
+            }
+            else if (dir.x <0)
+            {
+                Debug.Log("LEFT");
+            }
+            else
+            {
+                if(dir.z >0)
+                {
+                    Debug.Log("FRONT");
+                }
+                else if (dir.z < 0)
+                {
+                    Debug.Log("BACK");
+                }
+            }
+        }
+
+        if (col.gameObject.tag == "Track" || col.gameObject.tag == "Car")
+        {
+            if (col.relativeVelocity.magnitude > 25)
+            {
+                impactAmount = col.relativeVelocity.magnitude * 0.2f;
+                carHP -= impactAmount;
+                ContactPoint contact = col.contacts[0];
+                Quaternion rot = Quaternion.FromToRotation(contact.normal, Vector3.forward);
+                Vector3 pos = contact.point;
+                if (carHP > 0)
+                {
+                    //transform.DetachChildren();
+                    Instantiate(collisionParticle, pos, rot);
+                }
+                if(carHP <75)
+
+                {
+                    damagedParticle.SetActive(true);
+
+                }
+            }
+            if (carHP < 0)
+            {
+                //transform.DetachChildren();
+                carObject.SetActive(false);
+                Instantiate(DeathParticle, transform.position, transform.rotation);
+                Instantiate(mainCamera,transform.position,transform.rotation);
+                Debug.Log("Destroyed!");
+            }
+        }
+       // Debug.Log(col.relativeVelocity.magnitude);
+        //Debug.Log("Collided");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+     //   Debug.Log(carHP);
+      //  Debug.Log(impactAmount);
+    }
+}
