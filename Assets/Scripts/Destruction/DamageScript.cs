4a91557242f40121fadac8b8e5e7336718a5ca7c
Tin-Tin <kt-chin@hotmail.com>
2016-05-07 15:43:57 +0100

    Collision destruction working

--- Assets/Scripts/Destruction/DamageScript.cs
+++ Assets/Scripts/Destruction/DamageScript.cs
@@ -32,50 +32,61 @@ public class DamageScript : MonoBehaviour {
     {
         foreach (ContactPoint contact in col.contacts)
         {
-            if (col.collider == colliderArray[0]|| col.collider == colliderArray[1]|| col.collider == colliderArray[2])
-            {
-                Debug.Log("Collided at the front");
-                destroyHood();
-            }
-        }
+           // Debug.Log(contact.point);
+           
 
-        if (col.gameObject.tag == "Track" || col.gameObject.tag == "Car")
-        {
-            
-            //print("Points colliding: " + col.contacts.Length);
-            //print("First point that collided: " + col.contacts[0].point);
-            if (col.relativeVelocity.magnitude > 25)
-            {
-                impactAmount = col.relativeVelocity.magnitude * 0.2f;
-                carHP -= impactAmount;
-                ContactPoint contact = col.contacts[0];
-                Quaternion rot = Quaternion.FromToRotation(contact.normal, Vector3.forward);
-                Vector3 pos = contact.point;
-                if (carHP > 0)
-                {
-                    //transform.DetachChildren();
-                    Instantiate(collisionParticle, pos, rot);
-                }
-                if(carHP <75)
-                {
-                    damagedParticle.SetActive(true);
 
-                    //col.collider 
-                    if(col.collider==colliderArray[0]|| col.collider == colliderArray[1]|| col.collider == colliderArray[2])
+            if (col.gameObject.tag == "Track" || col.gameObject.tag == "Car")
+            {
+
+                //print("Points colliding: " + col.contacts.Length);
+                //print("First point that collided: " + col.contacts[0].point);
+                if (col.relativeVelocity.magnitude > 25)
+                {
+                    impactAmount = col.relativeVelocity.magnitude * 0.2f;
+                    carHP -= impactAmount;
+                    //ContactPoint contact = col.contacts[0];
+                    Quaternion rot = Quaternion.FromToRotation(contact.normal, Vector3.forward);
+                    Vector3 pos = contact.point;
+                    if (carHP > 0)
                     {
-                        GameObject door = GameObject.FindGameObjectWithTag("RightDoor");
-                        door.transform.parent = null;
-                        Debug.Log("COLLISION");
+                        //transform.DetachChildren();
+                        Instantiate(collisionParticle, pos, rot);
+                    }
+                    if (carHP < 75)
+                    {
+                        damagedParticle.SetActive(true);
+                        if (contact.thisCollider == colliderArray[0] || contact.thisCollider == colliderArray[1] || contact.thisCollider == colliderArray[2])
+                        {
+                            Debug.Log("Collided at the front");
+                            destroyHood();
+                        }
+                        if (contact.thisCollider == colliderArray[6] || contact.thisCollider == colliderArray[7])
+                        {
+                            Debug.Log("Back collision");
+                            destroyBumper();
+                        }
+                        if (contact.thisCollider == colliderArray[5])
+                        {
+                            Debug.Log("Left Door Broken");
+                            destroyLeftDoor();
+                        }
+                        if(contact.thisCollider ==  colliderArray[4])
+                        {
+                            Debug.Log("Right Door Broken");
+                            destroyRightDoor();
+                        }
+
                     }
                 }
-            }
-            if (carHP < 0)
-            {
-                //transform.DetachChildren();
-                carObject.SetActive(false);
-                Instantiate(DeathParticle, transform.position, transform.rotation);
-                Instantiate(mainCamera,transform.position,transform.rotation);
-               // Debug.Log("Destroyed!");
+                if (carHP < 0)
+                {
+                    //transform.DetachChildren();
+                    carObject.SetActive(false);
+                    Instantiate(DeathParticle, transform.position, transform.rotation);
+                    Instantiate(mainCamera, transform.position, transform.rotation);
+                    // Debug.Log("Destroyed!");
+                }
             }
         }
        // Debug.Log(col.relativeVelocity.magnitude);
@@ -84,14 +95,14 @@ public class DamageScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKeyUp(KeyCode.X))
-            {
-            destroyHood();
-            destroyLeftDoor();
-            destroyRightDoor();
-            destroyBumper();
-        }
-        //Debug.Log(carHP);
+        //if(Input.GetKeyUp(KeyCode.X))
+        //    {
+        //    destroyHood();
+        //    destroyLeftDoor();
+        //    destroyRightDoor();
+        //    destroyBumper();
+        //}
+        Debug.Log(carHP);
         //Debug.Log(impactAmount);
     }
     void destroyRightDoor()
