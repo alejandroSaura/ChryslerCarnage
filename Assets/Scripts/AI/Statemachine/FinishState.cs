d228a8d10baade40f4524d272ba80f34a9a30ba5
SokolMurturi121 <sokolm@live.co.uk>
2016-05-21 17:12:57 +0000

    split screen and AI

---
+++ Assets/Scripts/AI/Statemachine/FinishState.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishState : ILapState {
+
+    private readonly StatePatternLap LapState;
+
+    public FinishState(StatePatternLap statePatternLap)
+    {
+        LapState = statePatternLap;
+    }
+
+    public void UpdateState()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    public void ToStartState()
+    {
+
+    }
+
+    public void ToInLapState()
+    {
+
+    }
+
+    public void ToFinishState()
+    {
+
+    }
+}
---
+++ Assets/Scripts/AI/Statemachine/StatePatternLap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatePatternLap : MonoBehaviour {
+
+
+    [HideInInspector] public ILapState currentState;
+    [HideInInspector] public StartState startState;
+    [HideInInspector] public InLapState inLapState;
+    [HideInInspector] public FinishState finishState;
+
+    void Awake() {
+        startState = new StartState(this);
+        inLapState = new InLapState(this);
+        finishState = new FinishState(this);
+;    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
