4913048b5fcdbb47ce25e5f0072b939444b88637
alejandroSaura <alejandro.saura.villanueva@gmail.com>
2016-05-22 14:23:14 +0100

    Destruction wall and car respawner

--- Assets/Scripts/AI/FollowPathV2.cs
+++ Assets/Scripts/AI/FollowPathV2.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class FollowPathV2 : MonoBehaviour
 {
+    public bool followFromBehind = false;
+
     public Transform follower;
     float previousDistanceToFollower;
 
@@ -11,13 +13,15 @@ public class FollowPathV2 : MonoBehaviour
     float t;
 
     public float segmentLength = 0.003f;
-    float speed = 1f;
+    public float speed = 1f;
 
     // for dealing with arbitrary oriented splines
     Vector3 lastForward = Vector3.zero;
     public bool reverse = false;
 
     public float distanceSinceLapStart = 0;
+
+    public float desiredDistToFollower = 50;
 
     // Use this for initialization
     void Start()
@@ -95,19 +99,55 @@ public class FollowPathV2 : MonoBehaviour
 
     void AdjustSpeed()
     {
-        float distanceToFollower = Vector3.Distance(transform.position, follower.position);
-        float distanceToObjective = follower.parent.GetComponent<AICarMovementV3>().distToNode - distanceToFollower;
+        if (follower != null)
+        {
+            float distanceToFollower = 0;
+            float distanceToObjective = 0;
 
-        float derivate = 0;
-        if (Time.deltaTime != 0)
-            derivate = (distanceToObjective - previousDistanceToFollower) / Time.deltaTime;        
+            if (followFromBehind)
+            {
+                //speed = follower.transform.GetComponentInChildren<FollowPathV2>().speed;
 
-        speed += derivate * Time.deltaTime;
-        speed = Mathf.Clamp(speed, 0, float.MaxValue);
+                distanceToFollower = follower.transform.GetComponentInChildren<FollowPathV2>().distanceSinceLapStart - distanceSinceLapStart;
+                distanceToObjective = distanceToFollower - desiredDistToFollower;
 
-        if (distanceToObjective < -2) speed = 0.0001f;
+                //if (distanceToObjective > 0) speed += 0.05f * distanceToObjective;
+                //if (distanceToObjective < 0) speed += 0.1f * distanceToObjective;
 
-        previousDistanceToFollower = distanceToObjective;
+                speed = distanceToObjective;
+
+                speed = Mathf.Clamp(speed, 0, float.MaxValue);
+
+                return;
+            }
+            else
+            {
+                distanceToFollower = Vector3.Distance(transform.position, follower.position);
+                //if (isDeathWall) distanceToFollower *= -1;
+                //float distanceToObjective = follower.parent.GetComponent<AICarMovementV3>().distToNode - distanceToFollower;
+                distanceToObjective = desiredDistToFollower - distanceToFollower;
+            }           
+
+            float derivate = 0;
+            if (Time.deltaTime != 0)
+            {
+                derivate = (distanceToObjective - previousDistanceToFollower) / Time.deltaTime;
+                //if (isDeathWall && distanceToObjective < 0) derivate *= -1;
+            }
+
+            speed += derivate * Time.deltaTime;            
+
+            speed = Mathf.Clamp(speed, 0, float.MaxValue);            
+
+            previousDistanceToFollower = distanceToObjective;
+
+            //if (distanceToObjective < 0 && isDeathWall) speed = 0;
+                        
+        }
+        else
+        {
+            speed = 0.0001f;
+        }
     }
 
     void ChangeToNextSpline()
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,15 +8,28 @@ public class GameManager : MonoBehaviour {
     PathDecisor pathDecisor;
     List<CarRaceController> cars;
 
+    FollowPathV2 deathWallFollower;
+    FollowPathV2 carRespawner;
+
+    public float deathWallDistance = 4;
+    public float carRespawnerDistance = 2;
+
     public float[] positionPenalization = new float[4];
 
 
     void Start ()
     {
         cars = new List<CarRaceController>(GameObject.Find("Cars").GetComponentsInChildren<CarRaceController>());
-        
+
+        deathWallFollower = GameObject.Find("DeathWall").GetComponent<FollowPathV2>();
+        deathWallFollower.followFromBehind = true;
+        deathWallFollower.desiredDistToFollower = deathWallDistance;
 
         pathDecisor = gameObject.GetComponent<PathDecisor>();
+
+        carRespawner = GameObject.Find("CarRespawner").GetComponent<FollowPathV2>();
+        carRespawner.followFromBehind = true;
+        carRespawner.desiredDistToFollower = carRespawnerDistance;
 
         bool lapCalculated = false;
         while (!lapCalculated)
@@ -28,6 +41,7 @@ public class GameManager : MonoBehaviour {
 	void Update ()
     {
         SortCars();
+        UpdateDeathWall();
     }
 
     void SortCars()
@@ -40,5 +54,11 @@ public class GameManager : MonoBehaviour {
             cars[i].position = i + 1;
             cars[i].penalization = positionPenalization[i];
         }
+    }
+
+    void UpdateDeathWall()
+    {
+        deathWallFollower.follower = cars[0].transform;
+        carRespawner.follower = cars[0].transform;
     }
 }
